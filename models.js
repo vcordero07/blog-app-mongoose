2a501d06aad4bdfd5b968af06635adfe550e5661
vcordero07 <vcordero07@gmail.com>
2017-09-07 18:04:38 -0400

    wip

--- models.js
+++ models.js
@@ -1,74 +1,40 @@
 const uuid = require('uuid');
+const mongoose = require('mongoose');
 
-// This module provides volatile storage, using a `BlogPost`
-// model. We haven't learned about databases yet, so for now
-// we're using in-memory storage. This means each time the app stops, our storage
-// gets erased.
-
-// Don't worry too much about how BlogPost is implemented.
-// Our concern in this example is with how the API layer
-// is implemented, and getting it to use an existing model.
-
-
-function StorageException(message) {
-  this.message = message;
-  this.name = "StorageException";
-}
-
-const BlogPosts = {
-  create: function(title, content, author, publishDate) {
-    const post = {
-      id: uuid.v4(),
-      title: title,
-      content: content,
-      author: author,
-      publishDate: publishDate || Date.now()
-    };
-    this.posts.push(post);
-    return post;
+const blogPostSchema = mongoose.Schema({
+  author: {
+    firstName: String,
+    lastName: String
   },
-  get: function(id = null) {
-    // if id passed in, retrieve single post,
-    // otherwise send all posts.
-    if (id !== null) {
-      return this.posts.find(post => post.id === id);
-    }
-    // return posts sorted (descending) by
-    // publish date
-    return this.posts.sort(function(a, b) {
-      return b.publishDate - a.publishDate
-    });
+  title: {
+    type: String,
+    required: true
   },
-  delete: function(id) {
-    const postIndex = this.posts.findIndex(
-      post => post.id === id);
-    if (postIndex > -1) {
-      this.posts.splice(postIndex, 1);
-    }
+  content: {
+    type: String
   },
-  update: function(updatedPost) {
-    const {
-      id
-    } = updatedPost;
-    const postIndex = this.posts.findIndex(
-      post => post.id === updatedPost.id);
-    if (postIndex === -1) {
-      throw new StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`)
-    }
-    this.posts[postIndex] = Object.assign(
-      this.posts[postIndex], updatedPost);
-    return this.posts[postIndex];
+  created: {
+    type: Date,
+    default: Date.now
   }
-};
+});
 
-function createBlogPostsModel() {
-  const storage = Object.create(BlogPosts);
-  storage.posts = [];
-  return storage;
+blogPostSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
+
+blogPostSchema.methods.apiRepr = function() {
+  return {
+    id: this._id,
+    author: this.authorName,
+    content: this.content,
+    title: this.title,
+    created: this.created
+  };
 }
 
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 
 module.exports = {
-  BlogPosts: createBlogPostsModel()
+  BlogPost
 };
